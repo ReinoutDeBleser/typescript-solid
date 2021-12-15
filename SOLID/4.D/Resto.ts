d8f775a63d76fdac2f754da237be02c39e7e9ba3
RedMarkD <reinout.de.bleser@gmail.com>
2021-12-15 15:59:54 +0100

    refactored last letter of SOLID

---
+++ SOLID/4.D/Resto.ts
@@ -0,0 +1,114 @@
+interface Preparation {
+    _isOn : boolean;
+    on(): void;
+    off() : void;
+    cook(item: string);
+}
+class Restaurant {
+    private _name : string;
+    private _preparation : Preparation;
+
+    constructor(name : string, preparation) {
+        this._name = name;
+        this._preparation = preparation
+    }
+
+    public Cook(item : string) {
+        this._preparation.on();
+        this._preparation.cook(item);
+        this._preparation.off();
+    }
+}
+
+class Oven implements Preparation{
+    _isOn : boolean;
+
+    public on() : void
+    {
+        setTimeout(function (){
+            document.getElementById('target').innerHTML += "<p>"+new Date().getHours()+":"+new Date().getMinutes()+" : THE GAS IS ON!</p>";
+        }, 1000);
+        console.log("THE GAS IS ON!"); //insert fart humor here
+        this._isOn = true;
+    }
+
+    public off() : void
+    {
+        setTimeout(function (){
+            document.getElementById('target').innerHTML += "<p>"+new Date().getHours()+":"+new Date().getMinutes()+" : THE GAS IS OFF!</p><hr>";
+        }, 3000);
+        console.log("THE GAS IS OFF!"); //insert fart humor here
+        this._isOn = false;
+    }
+
+    public cook(item : string)
+    {
+        if(this._isOn) {
+            setTimeout(function (){
+                document.getElementById('target').innerHTML += "<p>"+new Date().getHours()+":"+new Date().getMinutes()+" : Now baking " + item + " !</p>";
+            }, 2000);
+            console.log("Now baking " + item + "!");
+        }
+        else
+        {
+            setTimeout(function (){
+                document.getElementById('target').innerHTML += "<p>"+new Date().getHours()+":"+new Date().getMinutes()+" : there is no gas!</p>";
+            }, 2000);
+            console.log("there is no gas!");//insert fart humor here
+        }
+    }
+}
+
+class Stove implements Preparation{
+    _isOn : boolean;
+
+    public on() : void
+    {
+        setTimeout(function (){
+            document.getElementById('target').innerHTML += "<p>"+new Date().getHours()+":"+new Date().getMinutes()+" : THE STOVE IS ON!</p>";
+        }, 1000);
+        console.log("THE STOVE IS ON!"); //insert fart humor here
+        this._isOn = true;
+    }
+
+    public off() : void
+    {
+        setTimeout(function (){
+            document.getElementById('target').innerHTML += "<p>"+new Date().getHours()+":"+new Date().getMinutes()+" : THE STOVE IS OFF!</p><hr>";
+        }, 3000);
+        console.log("THE STOVE IS OFF!"); //insert fart humor here
+        this._isOn = false;
+    }
+
+    public cook(item : string)
+    {
+        if(this._isOn) {
+            setTimeout(function (){
+                document.getElementById('target').innerHTML += "<p>"+new Date().getHours()+":"+new Date().getMinutes()+" : Now baking " + item + " !</p>";
+            }, 2000);
+            console.log("Now preparing " + item + "!");
+        }
+        else
+        {
+            setTimeout(function (){
+                document.getElementById('target').innerHTML += "<p>"+new Date().getHours()+":"+new Date().getMinutes()+" : there is no gas!</p>";
+            }, 2000);
+            console.log("there is no heat!");//insert fart humor here
+        }
+    }
+}
+
+let creperie = new Restaurant("Creperie", new Stove());
+creperie.Cook("crepes");
+
+
+// let bakery = new Restaurant("Bakery", new Oven());
+// bakery.Cook("cookies");
+
+//Now if we want to add a new restaurant with an ELECTRIC cooker, we are gonna be in a hot mess ...
+/*
+let bakery = new Restaurant("Bakery", new Oven());
+bakery.Cook("cookies");
+
+
+ */
